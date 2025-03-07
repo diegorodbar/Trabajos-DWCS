e5bdfd8b6f2df36704627af509ffedc5e32a43b8
Diego Rodríguez <diego25598@gmail.com>
2025-03-07 20:58:13 +0100

    add ud8 task

---
+++ ud8-ejercicio1/src/claves.inc.php
@@ -0,0 +1,7 @@
+<?php
+// Google API (Tasks.php)
+// $googleClientIdTarea8 = "377480346664-vfjdsj83emp34n75ikudneoc9e3h5so5.apps.googleusercontent.com";
+// $googleClientSecretTarea8 = "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";
+
+// Bing Maps API (Coordenadas.php)
+// $keyBing = "Au1tWYSz_y_rJsqDnJtAojXcFtljvRI5tF5-qBIC5-wmr_Rk9qwPsF_GmvVjgPy1";
